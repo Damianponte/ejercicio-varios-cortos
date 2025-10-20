24b6ac1fe34473ea2be01eb4f5caff7fff58df0f
dami <damianponte@hotmail.es>
2025-10-20 11:57:18 +0200

    empezando ejercicio puertas

---
+++ src/ejerciciosPuertas.java
@@ -0,0 +1,12 @@
+public class ejerciciosPuertas {
+    public static void main(String[] args) {
+
+//tenemos 3 puertas// elegir aleatoriamente donde esta el premio.
+        // cual elije el concursante
+        //abrir una de las otras 2
+        // y pedirle si quiere cambiar la puerta
+        // comprueba en 100 veces cuantas veces ha acertado al cambiar.
+
+
+    }
+}
