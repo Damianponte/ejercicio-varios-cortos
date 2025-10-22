f848fbfaebe8cd40a4d9246b57f996672130a2b7
dami <damianponte@hotmail.es>
2025-10-22 17:55:53 +0200

    iniciando ejercicio

--- src/ejerciciosPuertas.java
+++ src/ejerciciosPuertas.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class ejerciciosPuertas {
     public static void main(String[] args) {
 
@@ -6,6 +9,22 @@ public class ejerciciosPuertas {
         //abrir una de las otras 2
         // y pedirle si quiere cambiar la puerta
         // comprueba en 100 veces cuantas veces ha acertado al cambiar.
+
+        // Simular N partidas (por ejemplo, 100) del juego de las 3 puertas (Monty Hall)
+
+        // para comparar dos estrategias:
+
+        //   A) Mantener la puerta elegida inicialmente.
+
+        //   B) Cambiar a la otra puerta disponible después de que el presentador abra una.
+
+        int vecesrepetir;
+        System.out.println("cuantas rondas va a jugar");
+        Scanner lector=new Scanner(System.in);
+        vecesrepetir=lector.nextInt();
+
+
+
 
 
     }
